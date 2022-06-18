e9baae839dcadd5b9f64b2d30c6904f132331699
helperstop <akhilnani14@gmail.com>
2022-06-18 22:05:09 +0530

    added add method

--- DSASelector.cs
+++ DSASelector.cs
@@ -8,5 +8,9 @@ namespace DSAPackageDotNet
         {
             Console.WriteLine("This is the DSA Package");
         }
+        public static double Add(int a, int b)
+        {
+            return a+b;
+        }
     }
 }
